7d6505935b5dbfd441a3416ed64f8805ae2b6d13
Tamas Soos <soos.tamas@groupimd.com>
2019-03-14 14:20:29 +0000

    Adding final versions

--- src/classComponent.jsx
+++ src/classComponent.jsx
@@ -1,8 +1,75 @@
 import React, { Component } from "react";
 
+import { ThemeContext } from "./themeContext";
+import MousePosition from "./MousePosition";
+
 class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Harry",
+      surname: "Potter",
+      width: window.innerWidth
+    };
+  }
+
+  componentDidMount() {
+    document.title = `${this.state.name} ${this.state.surname}`;
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentDidUpdate() {
+    document.title = `${this.state.name} ${this.state.surname}`;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  handleResize = () => {
+    this.setState({
+      width: window.innerWidth
+    });
+  };
+
+  handleNameChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+
+  handleSurnameChange = event => {
+    this.setState({
+      surname: event.target.value
+    });
+  };
+
   render() {
-    return <div className="row">Full name</div>;
+    return (
+      <ThemeContext.Consumer>
+        {theme => (
+          <MousePosition>
+            {({ x, y }) => (
+              <div className={theme}>
+                Full name class
+                <input
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                />
+                <input
+                  value={this.state.surname}
+                  onChange={this.handleSurnameChange}
+                />
+                <div>{this.state.width}</div>
+                <div>
+                  {x} + {y}
+                </div>
+              </div>
+            )}
+          </MousePosition>
+        )}
+      </ThemeContext.Consumer>
+    );
   }
 }
 
--- src/hookComponent.jsx
+++ src/hookComponent.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+
+import { ThemeContext } from "./themeContext";
+import { useMousePosition } from "./MousePosition";
 
 function Card(props) {
-  return <div className="row">Full name</div>;
+  const nameInput = useInputForm("Rick");
+  const surnameInput = useInputForm("Sanchez");
+  const theme = useContext(ThemeContext);
+  const width = useWindowWidth();
+  const { x, y } = useMousePosition();
+
+  useDocumentTitle(`${nameInput.value} ${surnameInput.value}`);
+
+  return (
+    <div className={theme}>
+      Full name
+      <input {...nameInput} />
+      <input {...surnameInput} />
+      <div>{width}</div>
+      <div>
+        {x} + {y}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
+
+export function useInputForm(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  };
+}
+
+export function useDocumentTitle(title) {
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+}
+
+export function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return width;
+}
